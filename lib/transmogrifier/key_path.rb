ecd3420bae190ff21be38d3703c24143fe9b08c9
John Foley and Steve Greenberg <pair+jfoley+sgreenberg@pivotallabs.com>
2013-10-25 11:59:26 -0700

    cleanup

--- lib/transmogrifier/key_path.rb
+++ lib/transmogrifier/key_path.rb
@@ -5,17 +5,13 @@ module Transmogrifier
     end
 
     def find(path)
-      matches = find_hash_with_parent(path)
-
-      #puts "parent: ", parent.inspect
-      #puts "child: ", child.inspect
-      #puts "key:, ", key.inspect
+      matches = find_matches(path)
 
       matches.map(&:child)
     end
 
     def modify(path, &blk)
-      matches = find_hash_with_parent(path)
+      matches = find_matches(path)
       match = matches[0]
       parent, child, key = match.parent, match.child, match.key
       if parent.nil?
@@ -27,41 +23,22 @@ module Transmogrifier
 
     private
 
-    def find_hash_with_parent(path)
-      puts "path: ", path.inspect
-      #if path.match(/^\./) # starts_with .
-      #  keys = path.split(".")
-      #  matches = find_from_root_sub_hash(@hash, keys, [])
-      #else
-      #  keys = path.split(".")
-      #  matches = find_sub_hash(@hash, keys, [])
-      #end
-
+    def find_matches(path)
       keys = path.split(".")
       matches = []
-      start(@hash, keys, matches)
 
-      matches
-    end
-
-    def find_sub_hash(hash, keys, matches)
-      wip(nil, hash, keys, matches)
-      matches
-    end
-
-    def start(hash, keys, matches)
       root_match_only = false
       if keys[0] == ""
         root_match_only = true
         keys.shift
       end
-      wip(nil, hash, keys, matches, root_match_only)
+      find_sub_hashes(nil, @hash, keys, matches, root_match_only)
+
+      matches
     end
 
-    def wip(parent, hash, keys, matches, root_match_only)
-      puts "parent: #{parent.inspect}, hash: #{hash.inspect}, keys: #{keys.inspect}"
+    def find_sub_hashes(parent, hash, keys, matches, root_match_only)
       hash.each do |key, value|
-        puts "key: #{key.inspect}, value: #{value.inspect}"
         if key == keys[0]
           # possible match, keep going
           if ( keys.length == 1 )
@@ -69,53 +46,14 @@ module Transmogrifier
             matches << Match.new(parent, hash, key)
           else
             keys.shift
-            wip(hash, value, keys, matches, root_match_only) if value.is_a?(Hash)
+            find_sub_hashes(hash, value, keys, matches, root_match_only) if value.is_a?(Hash)
           end
         else
           next if root_match_only
           # go deeper
-          wip(hash, value, keys, matches, root_match_only) if value.is_a?(Hash)
+          find_sub_hashes(hash, value, keys, matches, root_match_only) if value.is_a?(Hash)
         end
       end
-    end
-
-    def find_from_sub_hash(hash, keys, matches)
-      # find the first "key" match.  recurse on each
-
-      puts "hash: ", hash.inspect
-      puts "keys: ", keys.inspect
-      current = hash
-      key = keys.shift
-      puts "current key: #{key}"
-      parent = current
-      current = current[key]
-
-
-
-    end
-
-    def find_from_root_sub_hash(hash, keys, matches)
-      keys.reject! { |k| k.nil? || k.empty? }
-      puts "hash: ", hash.inspect
-      puts "keys: ", keys.inspect
-      current = hash
-      key = keys.shift
-      puts "current key: #{key}"
-      parent = current
-      current = current[key]
-
-
-      if keys.length == 1
-        if ( current.has_key?(keys[0]))
-          matches << Match.new(parent, current, key) unless current.nil?
-        end
-      else
-        return [] if current.nil?
-        current.each do |k, sub_hash|
-          find_sub_hash(sub_hash, keys, matches)
-        end
-      end
-
     end
 
     class Match
