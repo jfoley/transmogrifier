1127052e931d54fa2a581918a4192618c8260074
Amit Kumar Gupta <amitkgupta84@gmail.com>
2013-11-03 00:38:18 -0700

    Remove unused Engine#add_rule

--- lib/transmogrifier/engine.rb
+++ lib/transmogrifier/engine.rb
@@ -1,32 +1,23 @@
 module Transmogrifier
   class Engine
-    def self.load_rules(rules_array)
-      engine = new
-      rules_array.each do |rule|
-        type = rule.delete("type").to_sym
-        selector = rule.delete("selector")
-        options = [rule["object"], rule["from"], rule["to"], rule["name"]].compact
-        engine.add_rule(type, selector, *options)
-      end
-      engine
+    def self.from_rules_array(rules_array)
+      new(
+        rules_array.map do |rule|
+          type = rule["type"].capitalize
+          selector = rule["selector"]
+          options = [rule["object"], rule["from"], rule["to"], rule["name"]].compact
+          Transmogrifier::Rules.const_get(type).new(selector, *options)
+        end
+      )
     end
 
-    def initialize
-      @rules = []
-    end
-
-    def add_rule(rule_type, selector, *options)
-      @rules << case rule_type
-        when :append then Transmogrifier::Rules::Append.new(selector, *options)
-        when :delete then Transmogrifier::Rules::Delete.new(selector, *options)
-        when :move   then Transmogrifier::Rules::Move.new(selector, *options)
-      end
+    def initialize(rules)
+      @rules = rules
     end
 
     def run(input_hash)
       output_hash = input_hash.dup
-      @rules.each{ |rule| output_hash = rule.apply!(output_hash) }
-
+      @rules.each { |rule| output_hash = rule.apply!(output_hash) }
       output_hash
     end
   end
